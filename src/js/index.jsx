6640d54bd01f60fd2fc17d2d6ecdfd3ae22d8aca
Ozgur <ozgr@live.com>
2021-02-07 23:46:42 +0300

    Add imperial unit

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,22 +1,23 @@
+/* eslint react/jsx-fragments: 0 */
+
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { Form, Input } from 'rfv'
+import { Form, Input, Select } from 'rfv'
 
 import './../css/style.scss'
 
-const validations = {
-  isInt: [
-    {
-      rule: 'isInt',
-      invalidFeedback: 'Please provide a value'
-    }
-  ]
-}
-
 const calculateBmi = props => {
-  const { height, weight } = props
-  const height2 = height / 100
-  const bmi = (weight / (height2 * height2)).toFixed(2)
+  const { units, feet, inches, pounds, centimeters, kilograms } = props
+
+  let bmi
+  if (units === 'imperial') {
+    const _inches = ((feet * 12) + parseInt(inches))
+    bmi = ((pounds * 703) / (_inches * _inches)).toFixed(2)
+  } else {
+    const _centimeters = centimeters / 100
+    bmi = (kilograms / (_centimeters * _centimeters)).toFixed(2)
+  }
+
   let status = ''
   if (bmi < 18.5) {
     status = 'underweight'
@@ -27,41 +28,107 @@ const calculateBmi = props => {
   } else {
     status = 'obese'
   }
+
   return { bmi, status }
 }
 
 const App = () => {
+  const [selectedUnits, setSelectedUnits] = useState('imperial')
   const [calculatedBmi, setCalculatedBmi] = useState({})
+  const [removeItems, setRemoveItems] = useState([])
+
   const formOnSubmit = res => {
     if (res.isFormValid) {
       setCalculatedBmi(calculateBmi(res.items))
     }
   }
 
+  const selectOnChange = res => {
+    const { value } = res.e.target
+    setSelectedUnits(value)
+  }
+
   return (
     <div id='app'>
-      <Form onSubmit={formOnSubmit}>
+      <Form
+        onSubmit={formOnSubmit}
+        removeItems={removeItems}
+      >
         <div className='formGroup'>
-          <label htmlFor='height'>Height: centimeters</label>
-          <Input
-            type='text'
-            id='height'
-            name='height'
-            placeholder='173'
-            validations={validations.isInt}
-          />
+          <label htmlFor='units'>Units</label>
+          <Select
+            id='units'
+            name='units'
+            value={selectedUnits}
+            onChange={selectOnChange}
+          >
+            <option value='imperial'>Imperial</option>
+            <option value='metric'>Metric</option>
+          </Select>
         </div>
 
-        <div className='formGroup'>
-          <label htmlFor='weight'>Weight: kilograms</label>
-          <Input
-            type='text'
-            id='weight'
-            name='weight'
-            placeholder='65'
-            validations={validations.isInt}
-          />
-        </div>
+        {
+          selectedUnits === 'imperial'
+            ? (
+                <React.Fragment>
+                  <div className='formGroup'>
+                    <label htmlFor='feet'>Height: feet - inches</label>
+                    <div className='row'>
+                      <div className='col'>
+                        <Input
+                          id='feet'
+                          type='tel'
+                          name='feet'
+                          placeholder='5'
+                        />
+                      </div>
+
+                      <div className='col'>
+                        <Input
+                          type='tel'
+                          id='inches'
+                          name='inches'
+                          placeholder='8'
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className='formGroup'>
+                    <label htmlFor='pounds'>Weight: pounds</label>
+                    <Input
+                      type='tel'
+                      id='pounds'
+                      name='pounds'
+                      placeholder='143'
+                    />
+                  </div>
+                </React.Fragment>
+              )
+            : (
+                <React.Fragment>
+                  <div className='formGroup'>
+                    <label htmlFor='centimeters'>Height: centimeters</label>
+                    <Input
+                      type='tel'
+                      id='centimeters'
+                      placeholder='173'
+                      name='centimeters'
+                    />
+                  </div>
+
+                  <div className='formGroup'>
+                    <label htmlFor='kilograms'>Weight: kilograms</label>
+                    <Input
+                      type='tel'
+                      id='kilograms'
+                      name='kilograms'
+                      placeholder='65'
+                    />
+                  </div>
+                </React.Fragment>
+              )
+        }
 
         <div className='formGroup'>
           <button>Calculate</button>
