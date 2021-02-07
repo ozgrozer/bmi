03fea287272165fc475da3352c6992d3e7e2067b
Ozgur <ozgr@live.com>
2021-02-07 21:23:30 +0300

    Add bmi table

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -47,6 +47,33 @@ const App = () => {
           <button>Calculate</button>
         </div>
       </Form>
+
+      <div className='bmiTable'>
+        <div className='tr thead'>
+          <div className='td'>BMI</div>
+          <div className='td'>Weight Status</div>
+        </div>
+
+        <div className='tr'>
+          <div className='td'>Below 18.5</div>
+          <div className='td'>Underweight</div>
+        </div>
+
+        <div className='tr'>
+          <div className='td'>18.5 - 24.9</div>
+          <div className='td'>Normal</div>
+        </div>
+
+        <div className='tr'>
+          <div className='td'>25.0 - 29.9</div>
+          <div className='td'>Overweight</div>
+        </div>
+
+        <div className='tr'>
+          <div className='td'>30.0 and above</div>
+          <div className='td'>Obese</div>
+        </div>
+      </div>
     </div>
   )
 }
